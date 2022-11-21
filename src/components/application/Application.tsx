a44866cb34bea97ef20f2910277ee976ed8208b4
a1x40v <a1x40v@gmail.com>
2022-11-21 12:57:42 +0300

    RTL queries

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -5,14 +5,24 @@ const Application = () => {
     <>
       <h1>Job application form</h1>
       <h2>Section 1</h2>
+      <p>All fields are mandatory</p>
+      <span title="close">X</span>
+      <img src="https://via.placeholder.com/150" alt="a person with a laptop" />
+      <div data-testid="custom-element">Custom HTML element</div>
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Alex"
+            onChange={() => {}}
+          />
         </div>
         <div>
           <label htmlFor="bio">Bio</label>
-          <textarea id="bio" />
+          <textarea id="bio" name="bio" />
         </div>
         <div>
           <label htmlFor="job-location">Job location</label>
