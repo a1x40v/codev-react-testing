6cb5f8bdaee8014138d3d7fb4ce2aa5f6289830c
a1x40v <a1x40v@gmail.com>
2022-11-21 16:16:06 +0300

    Debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Application from './components/application/Application';
 import Skills from './components/skills/Skills';
 
 function App() {
   return (
     <div className="App">
+      <Application />
       <Skills skills={['HTML', 'CSS', 'JavaScript']} />
     </div>
   );
