456f148d09e94403612f12488de1f03eee1e059c
a1x40v <a1x40v@gmail.com>
2022-11-23 12:23:08 +0300

    Keyboard interactions

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -39,4 +39,32 @@ describe('Counter', () => {
     const countElement = screen.getByRole('heading');
     expect(countElement).toHaveTextContent('2');
   });
+
+  test('renders a count of 10 after clicking the set button', async () => {
+    render(<Counter />);
+    const amountInput = screen.getByRole('spinbutton');
+    await user.type(amountInput, '10');
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole('button', {
+      name: 'Set',
+    });
+    await user.click(setButton);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('10');
+  });
+
+  test('elements are focused in the right order', async () => {
+    render(<Counter />);
+    const amountInput = screen.getByRole('spinbutton');
+    const setButton = screen.getByRole('button', {
+      name: 'Set',
+    });
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
