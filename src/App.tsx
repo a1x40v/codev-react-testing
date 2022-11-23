ef6f10e6ea048a3f54be7f3e3c0dcf48720546a3
a1x40v <a1x40v@gmail.com>
2022-11-23 11:05:00 +0300

    Pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Application from './components/application/Application';
-import Skills from './components/skills/Skills';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Application />
-      <Skills skills={['HTML', 'CSS', 'JavaScript']} />
+      <Counter />
     </div>
   );
 }
